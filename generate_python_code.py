0e7e07bd73607bb37c6665a79d06e87d350d6fe3
Rafael Cosman <rafaelcosman@alumni.stanford.edu>
2023-01-18 20:18:28 -0300

    adds generate_python_code.py

---
+++ generate_python_code.py
@@ -0,0 +1,19 @@
+from helper_functions import ask
+
+def generate_python_code(problem):
+    query = f"""
+Please generate python code to {problem}.
+- The code should be as concise as possible
+- It should minimize side effects and never take dangerous actions like deleting files etc.
+
+Python code:
+"""
+
+    output = ask(query)
+    print(f"Python Code for {problem}:")
+    print(output)
+
+    return output
+
+if __name__ == "__main__":
+    generate_python_code(input("What would you like to generate python code for? For example, you could say 'Accept an integer and list its factors'. "))
