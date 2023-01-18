ce097d33ebb5dcd95c3fac1b72203e8596b39010
Rafael Cosman <rafaelcosman@alumni.stanford.edu>
2023-01-18 20:27:13 -0300

    cleans up editor.py

--- editor.py
+++ editor.py
@@ -1,15 +1,14 @@
 from helper_functions import ask
 
 def editor(text):
-    query = f"""
+    output = ask(f"""
 Please edit the following text to be as clear and concise as possible:
 
 {text}
 
-"""
+""")
 
-    output = ask(query)
-    print(f"Edited version of input text:")
+    print("Edited version of input text:")
     print(output)
 
     return output
