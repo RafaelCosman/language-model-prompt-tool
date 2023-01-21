9f07e62d65b7e57177d9acbaa085edcccb02bd24
Rafael Cosman <rafaelcosman@alumni.stanford.edu>
2023-01-21 10:19:53 -0300

    still needs work, but significant improvement to general_solver.py

--- general_solver.py
+++ general_solver.py
@@ -4,118 +4,53 @@ from generate_python_code import generate_python_code
 from generate_textbook import generate_textbook
 from generate_play import generate_play
 from solve_math_problem_UNSAFE import solve_math_problem_UNSAFE
+from write_python_file import write_python_file
 
 def general_solver(prompt):
-    suggested_function = gpt(f"""
-We are trying to solve the following prompt:
+    # Load the collected documentation
+    with open("collected_docstrings.txt", "r") as f:
+        collected_docstrings = f.read()
+
+    # Ask GPT for a function and what to pass into it
+    function_to_call = gpt(f"""
+For reference, here is the documentation of this entire repo:
+
+{collected_docstrings}
+
+We now have a prompt and would like you to figure out which function in this repo should be used to solve the propmt, and what should be passed into that function
+
+For example, if the prompt was "Please generate for me a play about love", you would output:
+generate_play("Love")
+
+If the prompt was "Write me a journal article about technology" you would output:
+generate_article("Technology")
+
+If the prompt was "Write me a short story about aliens" you would notice that there is no function in the repo for generating short stories, and therefore you would output:
+write_python_file("generate_short_story.py", "Please write me a file that generates short stories based on a topic. It should rely on the gpt function (similar to generate_textbook() or generate_journal()) and should be based on the fundamental structure that short stories have (e.g. characters, a narrative arc, etc.).")
+
+If the prompt was "Please write me an anthology of stories about Exploration"
+You would NOT use the generate_textbook() function, because that function is specific for generating textbooks. It does not work for anthologies.
+You would instead output:
+write_python_file("generate_short_story_anthology.py", "Please write me a file that generates an anthology of short stories based on a topic. It should rely on the gpt function (similar to generate_textbook() or generate_journal()) and should be based on the fundamental structure that anthologies of short stories have.")
+
+Note that you CANNOT use functions that do not exist. For example, if you don't see a generate_business_plan() function in the docs, you cannot call it.
+You must instead use write_python_file() to generate that file for later use. 
+
+================
+Now here is the actual prompt we are trying to solve:
 
 {prompt}
 
-Which of the following functions might be useful to solve this problem?
+Please now give us the function and the input to that function. Please do not output anything else.
 
-A. generate_article
-B. generate_journal
-C. generate_python_code
-D. generate_textbook
-E. generate_play
-F. solve_math_problem_UNSAFE
-
-Suggested function (please respond with one of the above options):
+Function and input:
 """)
 
-    print("suggested_function:", suggested_function)
+    print(function_to_call)
 
-    if suggested_function == "A. generate_article":
-        topic = gpt(f"""
-The user has asked:
+    output = eval(function_to_call)
 
-{prompt}
-
-As one step of solving this, we are going to write an article using the generate_article function.
-
-What string should we pass into that function? It should be the topic of an article that will help us respond to the user's prompt.
-
-Topic:
-
-""")
-        output = generate_article(topic)
-    elif suggested_function == "B. generate_journal":
-        topic = gpt(f"""
-The user has asked:
-
-{prompt}
-
-As one step of solving this, we are going to write an entire journal using the generate_journal function.
-
-What string should we pass into that function? It should be a topic that will help us respond to the user's prompt.
-
-Topic:
-
-""")
-        output = generate_journal(topic)
-    elif suggested_function == "C. generate_python_code":
-        problem = gpt(f"""
-The user has asked:
-
-{prompt}
-
-As one step of solving this, we are going to write python code using the generate_python_code function.
-
-What string should we pass into that function? It should be the description of a problem or program that will help us respond to the user's prompt.
-
-Topic:
-
-""")
-        output = generate_python_code(problem)
-    elif suggested_function == "D. generate_textbook":
-        topic = gpt(f"""
-The user has asked:
-
-{prompt}
-
-As one step of solving this, we are going to write an textbook using the generate_textbook function.
-
-What string should we pass into that function? It should be the topic of a textbook that will help us respond to the user's prompt.
-
-Topic:
-
-""")
-        output = generate_textbook(topic)
-    elif suggested_function == "E. generate_play":
-        play_name = gpt(f"""
-The user has asked:
-
-{prompt}
-
-As one step of solving this, we are going to write an play using the generate_play function.
-
-What string should we pass into that function? It should be the topic of a play that will help us respond to the user's prompt.
-
-Topic:
-
-""")
-        output = generate_play(play_name)
-    elif suggested_function == "F. solve_math_problem_UNSAFE":
-        problem = gpt(f"""
-The user has asked:
-
-{prompt}
-
-As one step of solving this, we are going to solve a math problem using the solve_math_problem_UNSAFE function.
-
-What string should we pass into that function? It should be a math problem that will help us respond to the user's prompt.
-
-Problem:
-
-""")
-        output = solve_math_problem_UNSAFE(problem)
-    else:
-        output = "No suitable function found."
-
-    print(f"\nOutput for {prompt}:")
     print(output)
 
-    return output
-
 if __name__ == "__main__":
-    general_solver(input("What would you like me to solve? Please give me a problem in plain language, like 'Find the factorial of a number'. "))
+    general_solver(input("What would you like me to solve? Please give me a problem in plain language, like 'Write me a textbook on American History'. "))
